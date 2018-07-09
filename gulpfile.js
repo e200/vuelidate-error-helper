8adc962e15ef0181f3c9362e77db1852c4186a8f
Eleandro Duzentos <eleandro@inbox.ru>
2018-07-09 21:57:58 +0100

    Added gulp-rename

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
-let gulp  = require('gulp')
-    babel = require('gulp-babel')
+let gulp   = require('gulp')
+    babel  = require('gulp-babel')
     uglify = require('gulp-uglify')
+    rename = require('gulp-rename')
 
 gulp.task('default', function () {
   return gulp.src('./src/index.js')
     .pipe(babel())
     .pipe(uglify())
+    .pipe(rename('vuelidate-error-helper.min.js'))
     .pipe(gulp.dest('./dist'))
 })
