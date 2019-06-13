dcfaf7eae9995ea7e58ea4b4c1026e851829681a
Eleandro Duzentos <eleandro@inbox.ru>
2019-06-13 09:08:28 +0100

    Removed mixin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import mixin from './mixin'
-
 function install (Vue, options) {
   Vue.prototype.$VuelidateErrorHelper = {
     lang: options.lang
   }
 }
-
-export { mixin }
 
 export default install
